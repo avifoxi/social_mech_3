7d6073737ef958e5c8093fb2893ad9ad6004e1d9
avifoxi <frosenox@gmail.com>
2015-08-10 11:01:51 -0400

    and that was pretty easy...

--- app/assets/javascripts/public_figures.js
+++ app/assets/javascripts/public_figures.js
@@ -1,37 +1,50 @@
 $(document).ready(function(){
-
-  var topSegments = $('.thumbTop');
-  var bottomSegments = $('.thumbnail .caption');
-
-  var batches = [topSegments, bottomSegments];
-
-  batchNormalizeHeights(batches);
-
-  $( window ).resize(function() {
-    batchNormalizeHeights(batches);
+  var $grid = $('.masonryGrid');
+  $grid.masonry({
+    isAnimated: true,
+    itemSelector: '.masonryThumb'
+  });
+  $(window).resize(function(){
+    $grid.masonry();
   });
 });
 
-function maxHeight($collection){
-  var heights = $collection.map(function(i, el){ 
-    return $(el).height()
-  });
-  return _.max(heights);
-}
 
-function resizeHeightTo($collection, height){
-  $.each($collection, function(i, el){
-    $(el).height(height);
-  })
-}
 
-function normalizeSegments($collection){
-  var max = maxHeight($collection);
-  resizeHeightTo($collection, max);
-}
+// $(document).ready(function(){
 
-function batchNormalizeHeights($array){
-  _.each($array, function($collection){
-    normalizeSegments($collection);
-  });
-}
+//   var topSegments = $('.thumbTop');
+//   var bottomSegments = $('.thumbnail .caption');
+
+//   var batches = [topSegments, bottomSegments];
+
+//   batchNormalizeHeights(batches);
+
+//   $( window ).resize(function() {
+//     batchNormalizeHeights(batches);
+//   });
+// });
+
+// function maxHeight($collection){
+//   var heights = $collection.map(function(i, el){ 
+//     return $(el).height()
+//   });
+//   return _.max(heights);
+// }
+
+// function resizeHeightTo($collection, height){
+//   $.each($collection, function(i, el){
+//     $(el).height(height);
+//   })
+// }
+
+// function normalizeSegments($collection){
+//   var max = maxHeight($collection);
+//   resizeHeightTo($collection, max);
+// }
+
+// function batchNormalizeHeights($array){
+//   _.each($array, function($collection){
+//     normalizeSegments($collection);
+//   });
+// }
