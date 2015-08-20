298fc094d0cbd138f5f939577055834c9711f3c8
avifoxi <frosenox@gmail.com>
2015-08-20 13:36:41 -0400

    ensured convention of naming in initializers -- perhaps thats where inconsistent lookup comes from?

--- app/models/aggregator/facebook.rb
+++ app/models/aggregator/facebook.rb
@@ -1,11 +1,16 @@
 module Aggregator::Facebook
 
   def get_fb_id_from_page(title)
-    $fb_graph_api.get_page(title)['id']
+    begin 
+      found = fb_graph_api.get_page(title)
+      return found['id']
+    rescue 
+      nil
+    end
   end
 
   def get_facebook_feed(fb_id, num)
-    $fb_graph_api.get_connections(fb_id, 'feed') # possible to return limited amount in query ? untested
+    fb_graph_api.get_connections(fb_id, 'feed') # possible to return limited amount in query ? untested
   end
 
 end
