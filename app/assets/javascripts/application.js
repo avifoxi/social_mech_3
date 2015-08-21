0e4ce57b74536bf21232dd2db596a81809f30be7
avifoxi <frosenox@gmail.com>
2015-08-21 11:12:03 -0400

    add dev notes in application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,10 +18,11 @@
 //= require imagesloaded
 //= require moment
 //= require turbolinks
-//= require_tree .
-//= ./public_figures require_tree 
 
 var PreviewPageMaster = require('./public_figures/previewPageMaster.js');
+
+// dilemma ... do we convert js package management to NPM, clear global namespaces?
+// many libraries above... and not sure what will happen to bootstrap dependencies if i remove global jQuery 
 
 $(document).ready(function() {
   window.PreviewPageMaster = PreviewPageMaster;
