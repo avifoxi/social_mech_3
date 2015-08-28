7b253199c23eaa56fc28d91f2e0d17be710c640d
avifoxi <frosenox@gmail.com>
2015-08-28 11:35:10 -0400

    working on validator, ammend input on form to much return vals of typeof in js

--- app/assets/javascripts/public_figures_preview_page_app/utils/realTimeValidator.js
+++ app/assets/javascripts/public_figures_preview_page_app/utils/realTimeValidator.js
@@ -1,27 +1,45 @@
 'use strict';
 
 var RealTimeValidator = function( $inputs ) {
-  var _dataKeys = _($inputs) // this is maybe too clever ... filter jQ els, ensure they contain data-key attrs, and reduce to a single key-val map 
-    .filter( function(ip){
-      return $(ip).attr('data-key') !== undefined;
-    })
-    .reduce(function( collector, ip, n){
-      
-      var dataKey = JSON.parse( $(ip).attr('data-key') );
-      var name = $(ip).attr('name');
-      
+  var _dataKeys = _.reduce( $inputs, function( collector, ip, n){ // n is iterator -- function doesn't work unless this value is in the args 
+      var dataKey =  $(ip).attr('data-key') ;
       if ( dataKey ){
+        var name = $(ip).attr('name');
         _.assign(collector, {
-          [$(ip).attr('name')]: dataKey
-        });
-        return collector;
+          [ name ]: JSON.parse( dataKey )
+        }); 
       }
+      return collector; 
     }, {});
   
 
   this.check = function( input ){
-    _dataKeys;
+    var name = input.name(),
+      value = input.getValue(),
+      dataKey = _dataKeys[ name ],
+      expected = parseExpectation( dataKey.type_requirement ),
+      url = dataKey.validate_url;
+    
+    if ( typeof value !== expected ){
+      input.setValidity( false )
+      return;
+    }
+    if ( url ){
+
+    }
+    input.setValidity( true );
   };
+  function numFromString( string ){
+    
+  }
+  function parseExpectation( exp ){
+    // currently, some of the expectations are in form:
+    // type#detail 
+    // ala string#csv
+    // right now, we're not gonna worry about second value... though perhaps later we will
+    // anyhoo -- right now just return string, first value of split on #
+    return exp.split('#')[0];
+  }
 }
 
 module.exports = RealTimeValidator;
